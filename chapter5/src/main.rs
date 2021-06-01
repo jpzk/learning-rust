0d608d6d181a0c1ca92e40f3889769e2f65e7f4b
Jendrik <jendrik@madewithtea.com>
2021-06-01 11:47:33 +0200

    added code for chapter5

--- chapter5/src/main.rs
+++ chapter5/src/main.rs
@@ -12,8 +12,17 @@ struct Rectangle {
 }
 
 impl Rectangle { 
+    fn square(size: u32) -> Rectangle { 
+        Rectangle { 
+            width: size,
+            height: size
+        }
+    }
     fn area(&self) -> u32 { 
         self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
