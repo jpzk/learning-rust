5347a044e95f601c22360447bc9c294b1145ce8c
Jendrik <jendrik@madewithtea.com>
2021-06-05 16:14:48 +0200

    change standard error output for errors

--- chapter12/src/main.rs
+++ chapter12/src/main.rs
@@ -6,11 +6,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     if let Err(e) = chapter12::run(config) { 
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
